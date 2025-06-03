8ebfa4800a94b2a48b9c331a1ed1c542e734a658
hrishabh gohiya <hrishabh.gohiya@yash.com>
2025-06-03 16:46:05 +0530

    db update

--- order-microservice/app/db.py
+++ order-microservice/app/db.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import sessionmaker
 from app.models.order import Base
 from sqlalchemy import create_engine
 
-DATABASE_URL = "mysql+mysqlconnector://root:root@localhost:3306/order_db"
+DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/order_db"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
